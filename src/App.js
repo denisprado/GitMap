378244cda4169ea98d4d6a9d7a00dff1f1a3472e
Denis Forigo <denisforigo@gmail.com>
2019-05-14 19:53:39 -0300

    First comit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import './config/ReactotronConfig';
+import store from './store';
+
+import Routes from './routes';
+
+console.tron.log('testando');
+const App = () => (
+  <div className="App">
+    <Provider store={store}>
+      <Routes />
+    </Provider>
+  </div>
+);
+
+export default App;
